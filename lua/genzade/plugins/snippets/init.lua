4d6b1508e82e18d801e99c1891e3edf9776deb8e
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-09-27 12:26:30 +0100

    Fix snippets

--- lua/genzade/plugins/snippets/init.lua
+++ lua/genzade/plugins/snippets/init.lua
@@ -4,13 +4,22 @@ local config = function()
     return
   end
 
+  require('luasnip.loaders.from_vscode').lazy_load()
+
   luasnip.config.set_config({
     history = true,
     updateevents = 'TextChanged,TextChangedI',
   })
+
+  luasnip.filetype_extend('ruby', { 'rails' })
 end
 
 return {
   'L3MON4D3/LuaSnip',
+  -- follow latest release.
+  version = 'v2.*',
+  -- install jsregexp (optional!).
+  build = 'make install_jsregexp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
   config = config,
 }
