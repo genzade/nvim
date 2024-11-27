18fd2fd864a537bd09ebf62aad88f3d74f9641f2
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2024-11-27 01:05:38 +0000

    Add more autocmds

--- lua/genzade/core/autocmds/init.lua
+++ lua/genzade/core/autocmds/init.lua
@@ -70,11 +70,77 @@ autocmd({ 'BufEnter', 'ModeChanged' }, {
   callback = function()
     local mode = vim.api.nvim_get_mode().mode
     local mode_hl_group = mode_hl_groups[mode]
+
     if mode_hl_group == nil then
       mode_hl_group = mode_hl_groups['n']
     end
+
     local hl = vim.api.nvim_get_hl(0, { name = mode_hl_groups[mode], link = false })
+
     hl = vim.tbl_extend('force', { bold = true }, hl)
     vim.api.nvim_set_hl(0, 'CursorLineNr', hl)
   end,
+})
+
+-- Remove items from quickfix list.
+-- `dd` to delete in Normal
+-- `d` to delete Visual selection
+local function delete_qf_items()
+  local mode = vim.api.nvim_get_mode()['mode']
+
+  local start_idx
+  local count
+
+  if mode == 'n' then
+    -- Normal mode
+    start_idx = vim.fn.line('.')
+    count = vim.v.count > 0 and vim.v.count or 1
+  else
+    -- Visual mode
+    local v_start_idx = vim.fn.line('v')
+    local v_end_idx = vim.fn.line('.')
+
+    start_idx = math.min(v_start_idx, v_end_idx)
+    count = math.abs(v_end_idx - v_start_idx) + 1
+
+    -- Go back to normal
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes(
+        '<esc>', -- what to escape
+        true,    -- Vim leftovers
+        false,   -- Also replace `<lt>`?
+        true     -- Replace keycodes (like `<esc>`)?
+      ),
+      'x',       -- Mode flag
+      false      -- Should be false, since we already `nvim_replace_termcodes()`
+    )
+  end
+
+  local qflist = vim.fn.getqflist()
+
+  for _ = 1, count, 1 do
+    table.remove(qflist, start_idx)
+  end
+
+  vim.fn.setqflist(qflist, 'r')
+  vim.fn.cursor(start_idx, 1)
+end
+
+autocmd('FileType', {
+  group = augroup('custom'),
+  pattern = 'qf',
+  callback = function()
+    -- Do not show quickfix in buffer lists.
+    vim.api.nvim_buf_set_option(0, 'buflisted', false)
+
+    -- Escape closes quickfix window.
+    vim.keymap.set('n', '<ESC>', function()
+      return vim.cmd.cclose()
+    end, { buffer = true, remap = false, silent = true })
+
+    -- `dd` deletes an item from the list.
+    vim.keymap.set('n', 'dd', delete_qf_items, { buffer = true })
+    vim.keymap.set('x', 'd', delete_qf_items, { buffer = true })
+  end,
+  desc = 'Quickfix tweaks',
 })
