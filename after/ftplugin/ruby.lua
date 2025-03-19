6dd1f65f1ab36ff31b3b9c5c3150e17cb87bafd0
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-03-19 22:48:26 +0000

    ruby: improve pry breakpoint handling
    - Simplified indentation logic for adding pry breakpoints.
    - Optimized removal of pry breakpoints by avoiding line deletions.
    - Updated key mappings for adding and removing pry breakpoints:
      - `<Leader>k` to add a breakpoint.
      - `<Leader>K` to remove all breakpoints.
    - Added buffer-specific key mappings for better scoping.

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -7,11 +7,6 @@ opt.iskeyword:append('?')
 opt.iskeyword:append('!')
 opt.iskeyword:append('$')
 
-local function start_of_line(line)
-  local stripped = string.gsub(line, '^%s*(.-)$', '%1')
-  return string.len(line) - string.len(stripped)
-end
-
 local function is_binding_line(line)
   return string.match(line, 'pry') or string.match(line, 'binding.pry')
 end
@@ -19,29 +14,35 @@ end
 local function add_binding()
   local pos = vim.api.nvim_win_get_cursor(0)
   local cur_line = vim.api.nvim_buf_get_lines(0, pos[1] - 1, pos[1], true)[1]
-  local start = start_of_line(cur_line)
-  local text = string.rep(' ', start) .. "require 'pry'; binding.pry"
+  local indent = cur_line:match('^%s*') or ''
+  local text = indent .. "require 'pry'; binding.pry"
   vim.api.nvim_buf_set_lines(0, pos[1] - 1, pos[1] - 1, true, { text })
 end
 
 local function remove_bindings()
-  local deleted_count = 0
-  for i, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, true)) do
-    if is_binding_line(line) then
-      vim.api.nvim_buf_set_lines(0, i - 1 - deleted_count, i - deleted_count, false, {})
-      deleted_count = deleted_count + 1
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  local new_lines = {}
+  for _, line in ipairs(lines) do
+    if not is_binding_line(line) then
+      table.insert(new_lines, line)
     end
   end
+  vim.api.nvim_buf_set_lines(0, 0, -1, true, new_lines)
 end
 
 local opts = { silent = true, noremap = true }
-vim.keymap.set('n', '<Leader>9', add_binding, opts, { desc = 'Add pry debugger statement' })
 vim.keymap.set(
   'n',
-  '<Leader>0',
+  '<Leader>k',
+  add_binding,
+  { desc = 'Add pry breakpoint', buffer = vim.api.nvim_get_current_buf() }
+)
+vim.keymap.set(
+  'n',
+  '<Leader>K',
   remove_bindings,
   opts,
-  { desc = 'Remove all pry debugger statement' }
+  { desc = 'Remove all pry breakpoint', buffer = vim.api.nvim_get_current_buf() }
 )
 
 ----Surround
