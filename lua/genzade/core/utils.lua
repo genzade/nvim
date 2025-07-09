194b1357e678d7baf859bce857f1e2d1c2efdd67
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-07-09 10:09:02 +0200

    Remove commented-out code in sanitize_str
    The sanitize_str function had several lines of commented-out code
    related to whitespace handling. These lines were removed to improve
    code readability and maintainability. No functional changes were made.

--- lua/genzade/core/utils.lua
+++ lua/genzade/core/utils.lua
@@ -18,26 +18,6 @@ M.sanitize_str = function(str)
   str = (str:gsub('\n' .. indent, '\n')):gsub('^%s+', '')
 
   return str
-
-  -- str = str:gsub('^%s*\n', '')
-
-  -- -- Find the minimum leading whitespace
-  -- local min_indent = nil
-  -- for line in str:gmatch('([^\n]*)\n?') do
-  --   local leading_spaces = line:match('^(%s*)')
-  --   if #leading_spaces < #line then -- Ignore empty lines
-  --     min_indent = min_indent and math.min(min_indent, #leading_spaces) or #leading_spaces
-  --   end
-  -- end
-
-  -- -- Remove the common leading whitespace
-  -- if min_indent then
-  --   local pattern = string.rep(' ', min_indent)
-  --   str = str:gsub('\n' .. pattern, '\n')
-  --   str = str:gsub('^' .. pattern, '')
-  -- end
-
-  -- return str
 end
 
 return M
