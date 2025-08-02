512ac92f520a39ddc011a51a8261520ed2ddbbba
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-08-02 15:35:59 +0100

    Add breakpoint helpers for haml and js
    Add filetype plugins for Haml and JavaScript that provide key mappings
    to insert and remove debugging breakpoints. For Haml, this inserts or
    removes `binding.pry` lines; for JavaScript, it handles `debugger`
    statements. Key mappings are registered with which-key for both filetypes.

---
+++ after/ftplugin/javascript.lua
@@ -0,0 +1,44 @@
+local function is_binding_line(line)
+  return string.match(line, 'debugger') -- or string.match(line, 'binding.pry')
+end
+
+local function add_binding()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  local cur_line = vim.api.nvim_get_current_line()
+  local indent = cur_line:match('^%s*') or ''
+  local text = indent .. "debugger"
+  vim.api.nvim_buf_set_lines(0, pos[1] - 1, pos[1] - 1, true, { text })
+  vim.cmd('silent! write') -- Save the file silently
+end
+
+local function remove_bindings()
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  local new_lines = vim.tbl_filter(function(line)
+    return not is_binding_line(line)
+  end, lines)
+  vim.api.nvim_buf_set_lines(0, 0, -1, true, new_lines)
+  vim.cmd('silent! write') -- Save the file silently
+end
+
+local wk_ok, wk = pcall(require, 'which-key')
+if not wk_ok then
+  return
+end
+
+wk.add({
+  {
+    mode = { 'n' },
+    {
+      '<Leader>k',
+      add_binding,
+      desc = 'Add pry breakpoint',
+      remap = false,
+    },
+    {
+      '<Leader>K',
+      remove_bindings,
+      desc = 'Remove all pry breakpoint',
+      remap = false,
+    },
+  },
+})
