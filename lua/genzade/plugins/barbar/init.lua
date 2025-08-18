b5a14ed14ef8cec5541676595d336b9a11473a5d
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-08-18 21:41:18 +0100

    barbar: Add gitsigns icons and setup config
    Enhance barbar.nvim integration by adding gitsigns icons for buffer status.
    Update plugin setup to use barbar's configuration and ensure gitsigns is
    listed as a dependency. Also, improve keymap formatting for clarity.

--- lua/genzade/plugins/barbar/init.lua
+++ lua/genzade/plugins/barbar/init.lua
@@ -9,26 +9,41 @@ local config = function()
     return
   end
 
+  local barbar_ok, barbar = pcall(require, 'barbar')
+  if not barbar_ok then
+    return
+  end
+
+  barbar.setup({
+    icons = {
+      gitsigns = {
+        added = { enabled = true, icon = '+' },
+        changed = { enabled = true, icon = '~' },
+        deleted = { enabled = true, icon = '-' },
+      },
+    },
+  })
+
   wk.add({
     {
       mode = { 'n' },
-      { '<Leader>x', '<CMD>BufferClose<CR>', desc = 'Close the buffer' },
-      { '<C-TAB>', '<CMD>BufferPrevious<CR>', desc = 'Go to previous buffer' },
-      { '<S-TAB>', '<CMD>BufferNext<CR>', desc = 'Go to next buffer' },
+      { '<Leader>x', '<CMD>BufferClose<CR>',     desc = 'Close the buffer' },
+      { '<C-TAB>',   '<CMD>BufferPrevious<CR>',  desc = 'Go to previous buffer' },
+      { '<S-TAB>',   '<CMD>BufferNext<CR>',      desc = 'Go to next buffer' },
       { '<Leader>b', group = 'Buffer operations' },
       {
         '<Leader>bP',
         '<CMD>BufferPickDelete<CR>',
         desc = 'Delete buffer [P]icker (enter jump-mode)',
       },
-      { '<Leader>bd', '<CMD>BufferDelete<CR>', desc = '[D]elete current buffer' },
+      { '<Leader>bd', '<CMD>BufferDelete<CR>',  desc = '[D]elete current buffer' },
       {
         '<Leader>bD',
         '<CMD>BufferCloseAllButCurrent<CR>',
         desc = '[D]elete all but current buffer',
       },
-      { '<Leader>bl', tbuiltin.buffers, desc = 'List opened [B]uffers (Telescope)' },
-      { '<Leader>bp', '<CMD>BufferPick<CR>', desc = 'Buffer [P]icker (enter jump-mode)' },
+      { '<Leader>bl', tbuiltin.buffers,         desc = 'List opened [B]uffers (Telescope)' },
+      { '<Leader>bp', '<CMD>BufferPick<CR>',    desc = 'Buffer [P]icker (enter jump-mode)' },
       { '<Leader>br', '<CMD>BufferRestore<CR>', desc = '[R]estore last deleted/closed buffer' },
     },
   })
@@ -38,6 +53,7 @@ return {
   'romgrk/barbar.nvim',
   dependencies = {
     'folke/which-key.nvim',
+    'lewis6991/gitsigns.nvim',
     'kyazdani42/nvim-web-devicons',
     'nvim-telescope/telescope.nvim',
   },
