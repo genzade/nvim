dc4d0733fec087db3efe203b0b29541d309c989a
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-08-18 21:30:58 +0100

    Add lsp module to core setup
    Include 'lsp' in the core_modules list for automatic loading during setup.
    Refactor require statement to use string.format for improved readability.
    Switch from error to assert for module loading failures.

--- lua/genzade/core/init.lua
+++ lua/genzade/core/init.lua
@@ -1,13 +1,11 @@
 local M = {}
 
 M.setup = function()
-  local core_modules = { 'autocmds', 'globals', 'keymaps', 'settings' }
+  local core_modules = { 'autocmds', 'globals', 'keymaps', 'lsp', 'settings' }
 
   for _, module in ipairs(core_modules) do
-    local ok, err = pcall(require, 'genzade' .. '.' .. 'core' .. '.' .. module)
-    if not ok then
-      error('Error loading ' .. module .. '\n\n' .. err)
-    end
+    local ok, err = pcall(require, string.format('genzade.core.%s', module))
+    assert(ok, string.format('Error loading %s\n\n%s', module, err))
   end
 end
 
