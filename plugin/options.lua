c49356406080ca59f406f7347cb0b2effe4ef064
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-12-21 00:49:42 +0100

    Move options/settings

---
+++ plugin/options.lua
@@ -0,0 +1,28 @@
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = 'split'
+vim.opt.laststatus = 3
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.shiftwidth = 2
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_gb' }
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.timeoutlen = 600
+vim.opt.title = true
+vim.opt.titlestring = '%t%( %M%)%( (%{expand("%:~:h")})%)%a (nvim)'
+vim.opt.updatetime = 250
+vim.opt.winborder = 'rounded'
+
+-- -- Save undo history (may want this)
+-- vim.opt.undofile = true
