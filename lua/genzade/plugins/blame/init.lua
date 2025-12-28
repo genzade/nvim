a26babb37fbee251c00a22a9527867d70ac623ee
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2025-12-29 00:23:45 +0100

    BLAME: switch to opts and fix BlameToggle call
    Replaces custom config function with direct opts usage for blame.nvim.
    Also updates the BlameToggle command invocation to use vim.cmd.BlameToggle().

--- lua/genzade/plugins/blame/init.lua
+++ lua/genzade/plugins/blame/init.lua
@@ -1,25 +1,16 @@
-local config = function()
-  local blame_ok, blame = pcall(require, 'blame')
-  if not blame_ok then
-    return
-  end
-
-  blame.setup({
-    blame_opts = { '-w' },
-    date_format = '%d/%m/%y',
-  })
-end
-
 return {
   'FabijanZulj/blame.nvim',
   lazy = false,
   cmd = 'BlameToggle',
-  config = config,
+  opts = {
+    blame_opts = { '-w' },
+    date_format = '%d/%m/%y',
+  },
   keys = {
     {
       '<leader>B',
       function()
-        vim.cmd('BlameToggle')
+        vim.cmd.BlameToggle()
       end,
       desc = 'Toggle Git [B]lame',
     },
