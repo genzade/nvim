732b68b1f8c4a2ee7cb024f85ccfac766a8566c0
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2026-01-07 00:20:02 +0100

    FILETYPES: improve config file detection
    Enhance filetype detection for config files by adding support for .env and
    .var extensions, .env filename, and files under /etc/default/. This ensures
    proper syntax highlighting and tooling for various configuration files.

--- ftdetect/filetypes.lua
+++ ftdetect/filetypes.lua
@@ -1,10 +1,18 @@
 vim.filetype.add({
+  extension = {
+    env = 'config',
+    var = 'config',
+  },
   filename = {
+    ['.env'] = 'config',
     -- Set up for docker_compose_language_service, this lsp should only be
     -- for docker-compose files
     ['compose.yaml'] = 'yaml.docker-compose',
     ['compose.yml'] = 'yaml.docker-compose',
     ['docker-compose.yaml'] = 'yaml.docker-compose',
     ['docker-compose.yml'] = 'yaml.docker-compose',
+  },
+  pattern = {
+    ['.*/etc/default/.*'] = 'config',
   },
 })
