f6b2ceaaf334badd9f0425cd5f92c059483b7ba3
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2026-01-21 10:25:59 +0000

    REFACTOR: clean up autopairs config and remove yaml companion
    - Removed unused completion integration from nvim-autopairs config.
    - Deleted yaml_companion plugin setup as it is no longer working after
      Neovim 0.10.0 upgrade and related issues remain unresolved.

--- lua/genzade/plugins/nvim_autopairs/init.lua
+++ lua/genzade/plugins/nvim_autopairs/init.lua
@@ -5,16 +5,6 @@ local config = function()
   end
 
   npairs.setup({ enable_check_bracket_line = false })
-
-  -- TODO: sort this once completion plugins are installed
-  local has_cmp, cmp = pcall(require, 'cmp')
-  if not has_cmp then
-    return
-  end
-
-  local npairs_cmp = require('nvim-autopairs.completion.cmp')
-
-  cmp.event:on('confirm_done', npairs_cmp.on_confirm_done())
 end
 
 return {
