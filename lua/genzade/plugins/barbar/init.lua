9281f03d0e137eed535a9f1cbf430583e9f3b068
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2026-01-22 13:58:30 +0000

    BARBAR: use vim.cmd for buffer commands
    Replace string-based buffer command mappings with direct vim.cmd function
    calls for improved readability and consistency. Also update buffer group
    description to "Buffers" for clarity.

--- lua/genzade/plugins/barbar/init.lua
+++ lua/genzade/plugins/barbar/init.lua
@@ -27,24 +27,16 @@ local config = function()
   wk.add({
     {
       mode = { 'n' },
-      { '<Leader>x', '<CMD>BufferClose<CR>',     desc = 'Close the buffer' },
-      { '<C-TAB>',   '<CMD>BufferPrevious<CR>',  desc = 'Go to previous buffer' },
-      { '<S-TAB>',   '<CMD>BufferNext<CR>',      desc = 'Go to next buffer' },
-      { '<Leader>b', group = 'Buffer operations' },
-      {
-        '<Leader>bP',
-        '<CMD>BufferPickDelete<CR>',
-        desc = 'Delete buffer [P]icker (enter jump-mode)',
-      },
-      { '<Leader>bd', '<CMD>BufferDelete<CR>',  desc = '[D]elete current buffer' },
-      {
-        '<Leader>bD',
-        '<CMD>BufferCloseAllButCurrent<CR>',
-        desc = '[D]elete all but current buffer',
-      },
-      { '<Leader>bl', tbuiltin.buffers,         desc = 'List opened [B]uffers (Telescope)' },
-      { '<Leader>bp', '<CMD>BufferPick<CR>',    desc = 'Buffer [P]icker (enter jump-mode)' },
-      { '<Leader>br', '<CMD>BufferRestore<CR>', desc = '[R]estore last deleted/closed buffer' },
+      { '<Leader>b', group = 'Buffers' },
+      { '<Leader>x', vim.cmd.BufferClose, desc = 'Close the buffer' },
+      { '<C-TAB>', vim.cmd.BufferPrevious, desc = 'Go to previous buffer' },
+      { '<S-TAB>', vim.cmd.BufferNext, desc = 'Go to next buffer' },
+      { '<Leader>bP', vim.cmd.BufferPickDelete, desc = 'Delete buffer [P]icker (jump-mode)' },
+      { '<Leader>bd', vim.cmd.BufferDelete, desc = '[D]elete current buffer' },
+      { '<Leader>bD', vim.cmd.BufferCloseAllButCurrent, desc = '[D]elete all but current buffer' },
+      { '<Leader>bl', tbuiltin.buffers, desc = 'List opened [B]uffers (Telescope)' },
+      { '<Leader>bp', vim.cmd.BufferPick, desc = 'Buffer [P]icker (enter jump-mode)' },
+      { '<Leader>br', vim.cmd.BufferRestore, desc = '[R]estore last deleted/closed buffer' },
     },
   })
 end
