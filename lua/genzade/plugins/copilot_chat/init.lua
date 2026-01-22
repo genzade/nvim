f649a4647f9eb1846cb845b6db070d067cce712a
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2026-01-22 12:34:47 +0000

    CopilotChat: Migrate keymaps to which-key
    Refactored Copilot Chat plugin keybindings to use which-key for better
    discoverability and organization. Added which-key as a dependency and
    moved all keymap definitions into a structured which-key registration
    inside the config function. This improves maintainability and user
    experience.

--- lua/genzade/plugins/copilot_chat/init.lua
+++ lua/genzade/plugins/copilot_chat/init.lua
@@ -3,27 +3,42 @@ return {
   dependencies = {
     { 'zbirenbaum/copilot.lua' },
     { 'nvim-lua/plenary.nvim', branch = 'master' },
+    { 'folke/which-key.nvim' },
   },
   build = 'make tiktoken',
   opts = {
     -- See Configuration section for options
   },
-  keys = {
-    { '<leader>ic', '<CMD>CopilotChat<CR>', mode = 'n', desc = 'Chat with Copilot' },
-    { '<leader>ic', '<CMD>CopilotChat<CR>', mode = 'v', desc = 'Chat with Copilot' },
-    { '<leader>id', '<CMD>CopilotChatDocs<CR>', mode = 'v', desc = 'Generate Docs' },
-    { '<leader>ie', '<CMD>CopilotChatExplain<CR>', mode = 'v', desc = 'Explain Code' },
-    { '<leader>if', '<CMD>CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
-    { '<leader>im', '<CMD>CopilotChatCommit<CR>', mode = 'n', desc = 'Generate Commit Message' },
-    { '<leader>io', '<CMD>CopilotChatOptimize<CR>', mode = 'v', desc = 'Optimize Code' },
-    { '<leader>ir', '<CMD>CopilotChatReview<CR>', mode = 'v', desc = 'Review Code' },
-    {
-      '<leader>is',
-      '<CMD>CopilotChatCommit<CR>',
-      mode = 'v',
-      desc = 'Generate Commit for Selection',
-    },
-    { '<leader>it', '<CMD>CopilotChatToggle<CR>', mode = 'n', desc = 'Toggle Copilot Chat' },
-    { '<leader>it', '<CMD>CopilotChatTests<CR>', mode = 'v', desc = 'Generate Tests' },
-  },
+  config = function()
+    require('CopilotChat').setup()
+    local wk_ok, wk = pcall(require, 'which-key')
+    if not wk_ok then
+      return
+    end
+
+    wk.add({
+      {
+        { '<leader>i', group = 'Copilot Chat' },
+        {
+          mode = { 'n', 'v' },
+          { '<leader>ic', vim.cmd.CopilotChat, desc = 'Chat with Copilot' },
+          { '<leader>it', vim.cmd.CopilotChatToggle, desc = 'Toggle Copilot Chat' },
+        },
+        {
+          mode = 'n',
+          { '<leader>im', vim.cmd.CopilotChatCommit, desc = 'Generate Commit msg' },
+        },
+        {
+          mode = 'v',
+          { '<leader>id', vim.cmd.CopilotChatDocs, desc = 'Generate Docs' },
+          { '<leader>ie', vim.cmd.CopilotChatExplain, desc = 'Explain Code' },
+          { '<leader>if', vim.cmd.CopilotChatFix, desc = 'Fix Code Issues' },
+          { '<leader>im', vim.cmd.CopilotChatCommit, desc = 'Generate commit msg for selection' },
+          { '<leader>io', vim.cmd.CopilotChatOptimize, desc = 'Optimize Code' },
+          { '<leader>ir', vim.cmd.CopilotChatReview, desc = 'Review Code' },
+          { '<leader>it', vim.cmd.CopilotChatTests, desc = 'Generate Tests' },
+        },
+      },
+    })
+  end,
 }
