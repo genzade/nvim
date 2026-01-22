f066f49722f5410da5ae8967de58908344283a06
Hamid Zadeh <12967144+genzade@users.noreply.github.com>
2026-01-22 12:45:32 +0000

    FLASH: inline keymaps and remove defaults module
    Moved the Flash.nvim keymap definitions directly into the plugin
    specification, eliminating the separate defaults module. This simplifies
    the configuration by keeping related logic together and reduces
    indirection. No functional changes to key mappings.

--- lua/genzade/plugins/flash/init.lua
+++ lua/genzade/plugins/flash/init.lua
@@ -1,8 +1,19 @@
-local defaults = require('genzade.plugins.flash.defaults')
-
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   opts = {},
-  keys = defaults.keys,
+  keys = function()
+    local ok, flash = pcall(require, 'flash')
+    if not ok then
+      return
+    end
+
+    return {
+      { '<Leader>s', mode = { 'n', 'x', 'o' }, flash.jump, desc = 'Flash' },
+      { '<Leader>S', mode = { 'n', 'x', 'o' }, flash.treesitter, desc = 'Flash Treesitter' },
+      { 'r', mode = 'o', flash.remote, desc = 'Remote Flash' },
+      { 'R', mode = { 'o', 'x' }, flash.treesitter_search, desc = 'Treesitter Search' },
+      { '<c-s>', mode = { 'c' }, flash.toggle, desc = 'Toggle Flash Search' },
+    }
+  end,
 }
